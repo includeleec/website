027bf9c7c655e46506597d17e626015404d7de99
leec <includeleec@gmail.com>
2020-04-28 03:06:43 +0800

    add html template

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  title: 'Nax.one - 致力于打造一流的星云链 PoD 节点服务',
+  favicon: '/assets/favicon.ico',
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [{ path: '/', component: '@/pages/index' }],
-  favicon: '/assets/favicon.ico',
 });
